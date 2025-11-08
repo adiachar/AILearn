f09f42b73e476501c39fa6e5add430f239d1cc67
Adithya_G <adithyaacharya710@gmail.com>
2025-11-08 20:46:33 +0530

    added Ip address 0.0.0.0 for port binding

--- backend/server/src/server.js
+++ backend/server/src/server.js
@@ -17,6 +17,6 @@ app.use('/api/ai/', aiVideoGenerationRouter)
 app.use('/api/auth', authenticationRouter)
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running in the port ${PORT}`)
 })
